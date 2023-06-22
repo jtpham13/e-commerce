159bf14bc839c529f5e7668ecb1d913f4ace6c7d
Joseph Pham <joeypham13@gmail.com>
2023-06-22 18:49:55 -0400

    fixed server.js routes and connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const routes = require("./routes");
-const sequelize = require("./config/connection");
+const routes = require("./routes/api");
+const sequelize = require("./config/");
 // import sequelize connection
 
 const app = express();

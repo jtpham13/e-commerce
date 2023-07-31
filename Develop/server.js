49247362069c56a580d4ce0ed37935da2b6022f3
Joseph Pham <joeypham13@gmail.com>
2023-07-31 16:53:37 -0400

    added routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const routes = require("./routes/api");
-const sequelize = require("./config/");
+const routes = require("./routes");
+const sequelize = require("./config/connection.js");
 // import sequelize connection
 
 const app = express();
